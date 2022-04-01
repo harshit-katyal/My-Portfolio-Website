0dcd6adb0b90f778539f7a17517ae81e7be9a870
harshit-katyal <hkatyal345@gmail.com>
2022-04-02 04:14:55 +0530

    final commit

---
+++ client/src/PortfolioContainer/Home/Profile/Profile.js
@@ -0,0 +1,72 @@
+import React from "react";
+import Typical from "react-typical";
+import ScrollService from "../../../utilities/ScrollService";
+import "./Profile.css";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a href="https://github.com/harshit-katyal/">
+                <i className="fa fa-github-square" />
+              </a>
+              <a href="">
+                <i className="fa fa-instagram" />
+              </a>
+              <a href="www.linkedin.com/in/harshit-katyal">
+                <i className="fa fa-linkedin-square" />
+              </a>
+              <a href="https://twitter.com/harshitkatyal12">
+                <i className="fa fa-twitter" />
+              </a>
+            </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'M <span className="highlighted-text">Harshit</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Enthusiastic Dev 😎",
+                    1000,
+                    "Full stack Developer 💻",
+                    1000,
+                    "Mern stack Dev 📱",
+                    1000,
+                    "React/React Native 🌐",
+                    1000,
+                  ]}
+                />
+              </h1>
+            </span>
+            <span className="profile-role-tagline">
+              Knack of building applications with front and back end operations.
+            </span>
+          </div>
+
+          <div className="profile-options">
+            <button className="btn primary-btn"
+            onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
+            > Hire Me </button>
+            <a href="resume.pdf" download="resume.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
